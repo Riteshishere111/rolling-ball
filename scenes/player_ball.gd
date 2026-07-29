9fbf50b493d2c93920a6d7ecfb33fc435d87f268
addhyan94 <addhyant85@gmail.com>
2026-07-29 11:49:48 +0530

    Add Walls - Right and Left only ..

---
+++ scenes/player_ball.gd
@@ -0,0 +1,19 @@
+extends RigidBody3D
+@export var move_force := 20.0 
+func _physics_process(delta):
+	var direction = Vector3.ZERO
+	
+	if Input.is_action_pressed("ui_right"):
+		direction.x+= 1
+	
+	elif Input.is_action_pressed("ui_left"):
+		direction.x+= -1
+	
+	elif Input.is_action_pressed("ui_up"):
+		direction.z-= 1
+	
+	elif Input.is_action_pressed("ui_down"):
+		direction.z+= 1
+	
+	elif direction!= Vector3.ZERO:
+		apply_central_force()direction.normalized()*move_force
